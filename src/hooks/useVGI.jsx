2f174a784c9aa19cd8e438bde0c6be1c7e3d22ec
Mark Vincent Pausanos <work.markpausanos@gmail.com>
2024-05-09 23:07:27 +0800

    Try

---
+++ src/hooks/useVGI.jsx
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+
+export const useVGI = (firstName, lastName) => {
+	const url = "https://608search.vgitraining.com/epasearch.php";
+
+	const [vgiData, setVgiData] = useState("");
+	const [isLoading, setIsLoading] = useState(true);
+
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const response = await fetch(url, {
+					method: "GET",
+					headers: {
+						"Content-Type": "application/json",
+					},
+				});
+
+				if (!response.ok) {
+					throw new Error("Network response was not ok");
+				}
+				const data = await response.text();
+				setVgiData(data);
+				setIsLoading(false);
+			} catch (error) {
+				console.error(error);
+				setIsLoading(false);
+			}
+		};
+		fetchData();
+	}, [firstName, lastName, url]);
+
+	return { vgiData, isLoading };
+};
